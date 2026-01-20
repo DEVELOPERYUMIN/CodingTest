f50559a62541479823a0c4194f35965564df8fbd
DEVELOPERYUMIN <147611458+DEVELOPERYUMIN@users.noreply.github.com>
2026-01-20 23:58:10 +0900

    [level 0] Title: 문자열 돌리기, Time: 13.52 ms, Memory: 7.36 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.py
@@ -0,0 +1,13 @@
+
+print("\n".join(input())) # join : 문자사이에 줄바꿈 삽입 
+
+
+# print("\n".join([ch for ch in s]))
+# print(*s,sep='\n')  * > 문자열을 문자단위로 품 
+
+'''
+왜 안대쥬?
+s=input()
+for ch in s:
+    print(ch)
+'''
