59c2f879b83ba9df25fa264317b05d2daaf2a39e
DEVELOPERYUMIN <147611458+DEVELOPERYUMIN@users.noreply.github.com>
2026-01-30 17:24:02 +0900

    Update 성분으로 구분한 아이스크림 총 주문량.sql

--- MySQL/프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ MySQL/프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -6,3 +6,15 @@ JOIN ICECREAM_INFO i
   ON f.FLAVOR = i.FLAVOR
 GROUP BY i.INGREDIENT_TYPE
 ORDER BY TOTAL_ORDER ASC;
+
+
+'''
+같은코드 / inner join 할 때는 where 쓰는게 더 직관적 
+    outerjoin 은 join 써야함 
+select i.ingredient_type, sum(f.total_order) as total_order
+from first_half f, icecream_info i
+where f.flavor = i.flavor
+group by i.ingredient_type
+order by total_order asc
+
+'''
